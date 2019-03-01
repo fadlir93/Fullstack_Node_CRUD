790a8bf6809439e60ec5a1be3fcca63ff32d746e
Fadli Ramadhan <fadlir93@gmail.com>
2019-03-01 21:22:35 +0700

    Config Express Handlebars and set default layout

--- server.js
+++ server.js
@@ -1,13 +1,20 @@
 require('./models/db');
 
 const express = require('express');
+const path = require('path');
+const exphbs = require('express-handlebars')
 
 const employeeController = require('./controllers/employeeController');
 
 var app = express();
 
+app.set('views', path.join(__dirname, '/views/'));
+app.engine('hbs', exphbs({ extname: 'hbs', defaultLayout: 'mainLayout', layoutsDir: __dirname + '/views/layouts/' }))
+app.set('view engine', 'hbs')
+
 app.listen(3000, () => {
     console.log('Express server started at : PORT 3000')
 })
 
+//route localhost:3000/employee
 app.use('/employee', employeeController)
