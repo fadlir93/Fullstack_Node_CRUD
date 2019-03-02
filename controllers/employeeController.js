30fae60bb204fd6a379f1443a49aa5f232f3dd41
Fadli Ramadhan <fadlir93@gmail.com>
2019-03-02 07:16:11 +0700

    Validation Email and required name

--- controllers/employeeController.js
+++ controllers/employeeController.js
@@ -23,7 +23,15 @@ function insertRecord(req, res) {
         if(!err) {
             res.redirect('employee/list')
         } else {
-            console.log("Error during record insertion" + err)
+            if (err.name == 'ValidationError'){
+                handleValidationError(err, req.body);
+                res.render('employee/addOrEdit', {
+                    viewTitle: 'Insert Employee',
+                    employee: req.body
+                });
+            } else {
+                console.log("Error during record insertion" + err)
+            }
         }
     });
 }
@@ -31,5 +39,20 @@ function insertRecord(req, res) {
 router.get('/list', (req, res) => {
     res.json('from list')
 })
+
+function handleValidationError(err, body) {
+    for(field in err.errors) {
+        switch(err.errors[field].path){
+            case 'fullName':
+                body['fullNameError'] = err.errors[field].message;
+                break;
+            case 'email':
+                body['emailError'] = err.errors[field].message;
+                break;
+            default:
+                break;
+        }
+    }
+}
 
 module.exports = router;
